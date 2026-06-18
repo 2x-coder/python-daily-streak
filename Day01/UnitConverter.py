57f2d69f472d6b0b4474ae17bd6cec1bbd665f89
abhishek8001 <abhisheknigam20006@gmail.com>
2026-06-18 22:49:53 +0530

    improved the unit converter
    improved the unit converter to convert atm to pascal also along with degree Celsius to Kelvin
    and
    formatted the yesterdays code

--- Day01/UnitConverter.py
+++ Day01/UnitConverter.py
@@ -4,11 +4,19 @@
 #foot inches
 #poise centipoise pascal
 #
-print("---------°C - K converter-------")
-# c = input("enter temp in degree celcius") 
-# --------this is wrong as "c is taken as a string"----------------
-c = float(input("enter temp in degree celcius")) 
-print(c , "°C = ", end="")
-K = c + 273.15
-print(K, "K")
 
+# #1
+# print("---------°C -> K converter-------")
+# # c = input("enter temp in degree celcius") 
+# # --------this is wrong as "c is taken as a string"----------------
+# c = float(input("enter temp in degree celcius")) 
+# print(c , "°C = ", end="")
+# K = c + 273.15
+# print(K, "K")
+
+#2
+print("---------atm -> Pa converter-------")
+atm = float(input("enter pressure in atm: ")) 
+print(atm , "atm = ", end = "")
+pascal = 101325*atm
+print(pascal, "Pa")
