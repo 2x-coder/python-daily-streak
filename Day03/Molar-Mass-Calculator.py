4915ed05eabdc106507565b8e67b07391ad79e96
abhishek8001 <abhisheknigam20006@gmail.com>
2026-06-20 00:40:36 +0530

    Update Molar-Mass-Calculator.py
    completed simple code for molar mass calculator
    will update more in the future

--- Day03/Molar-Mass-Calculator.py
+++ Day03/Molar-Mass-Calculator.py
@@ -3,10 +3,23 @@ elements = {
     "H": 1,
     "He": 4,
     "Li": 6,
-    "Be"
-    "B"
+    "Be": 9,
+    "B": 10,
     "C": 12,
-    "N"
+    "N": 14,
     "O": 16,
 }
 
+print("Molar Mass Calculator")
+
+h = int(input("number of h atoms:"))
+c = int(input("number of c atoms:"))
+o = int(input("number of o atoms:"))
+
+molar_mass = (
+    h*elements["H"]+
+    c*elements["C"]+
+    o*elements["O"]
+)
+
+print("Molar Mass =", molar_mass, "g/mol")
