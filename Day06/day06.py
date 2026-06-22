0e45450ad8281ca9c744661045a9eb98936676a3
abhishek8001 <abhisheknigam20006@gmail.com>
2026-06-22 23:55:20 +0530

    pandas
    started learning pandas

---
+++ Day06/day06.py
@@ -0,0 +1,20 @@
+# 1 create data/experiment.csv
+# 2 read csv using pandas
+import pandas as pd
+data = pd.read_csv("data/experiment.csv")
+print(data)
+
+# 3 Average Temperature 
+import pandas as pd 
+data = pd.read_csv("data/experiment.csv")
+average_temp = data["temperature"].mean()
+print("Average Temperature:", average_temp)
+
+#4 Maximum Temperature
+import pandas as pd
+data = pd.read_csv("data/experiment.csv")
+max_temp = data["temperature"].max()
+print("Maximum Temperature:", max_temp)
+
+#More examples to practice 
+#1 Minimum Temperature
