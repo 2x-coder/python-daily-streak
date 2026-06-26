68ade7664998b2e2851a2c01b8196d64e24857d3
abhishek8001 <abhisheknigam20006@gmail.com>
2026-06-26 23:58:46 +0530

    temperature converter again
    temperature converter again

---
+++ Day10/file.py
@@ -0,0 +1,24 @@
+'''Temperature Converter(Easy)
+generate a menu to choose from 1234
+1. celcius to kelvin
+2. kelvin to celsius
+3. celsius to fahrenheit
+4. exit
+
+'''
+print("========Temperature Converter========")
+print('''1. celcius to kelvin
+2. kelvin to celsius
+3. celsius to fahrenheit
+4. exit''')
+choice = input("enter your choice:")
+if choice =="1" :
+    print("enter celsius:")
+elif choice =="2":
+    print("enter kelvin:")
+elif choice == "3":
+    print("enter celsius:")
+elif choice == "4":
+    print("=============program ended==========")
+    
+
